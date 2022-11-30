6e392fd9977a2290f015a480bcbf03dacf2df52c
Monica Hanson <wolfmurdurer23@gmail.com>
2022-11-29 23:25:00 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Monica Hanson on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
